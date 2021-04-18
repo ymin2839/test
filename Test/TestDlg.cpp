b3d6dc6a27b9d638b8734e0de092cef2ae635cb2
ymin <ymin2839@gmail.com>
2021-04-18 20:37:29 +0900

    마스터

---
+++ Test/TestDlg.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include "framework.h"
+#include "Test.h"
+#include "TestDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+CTestDlg::CTestDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_TEST_DIALOG, pParent)
+{
+}
+
+void CTestDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CTestDlg, CDialogEx)
+	ON_WM_TIMER()
+END_MESSAGE_MAP()
+
+BOOL CTestDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	SetTimer(100, 2000, NULL);
+	SetTimer(100, 5000, NULL);
+
+	return TRUE;
+}
+
+
+void CTestDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	if (nIDEvent == 100)
+	{
+		AfxMessageBox(L"hihi");
+	}
+}
---
+++ Test/TestDlg.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+class CTestDlg : public CDialogEx
+{
+public:
+	CTestDlg(CWnd* pParent = nullptr);
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);
+
+protected:
+	virtual BOOL OnInitDialog();
+
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+
+	DECLARE_MESSAGE_MAP()
+};
